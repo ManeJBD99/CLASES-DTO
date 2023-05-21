8259d5c589443dc8f45d59ca70f89c95fea4c722
ManeJBD99 <132291296+ManeJBD99@users.noreply.github.com>
2023-05-21 11:51:44 -0500

    Update musica.java

--- src/clases/dto/musica.java
+++ src/clases/dto/musica.java
@@ -9,5 +9,57 @@ package clases.dto;
  * @author manuc
  */
 public class musica {
+        private String autor;
+    private String emisora;
+    private String grupo;
+    private String reconocimiento;
+    private String categoria;
+    private String producccion;
     
+    public String getAutor(){
+        return autor;
+    }
+    
+    public void setAutor(String autor){
+        this.autor = autor;
+    }
+    
+    public String getEmisora(){
+        return emisora;
+    }
+    public void setApellido(String emisora){
+        this.emisora = emisora;
+    }
+    
+    public String getGrupo(){
+        return grupo;
+    }
+    
+    public void setEdad(String grupo){
+        this.grupo = grupo;
+    }
+    
+    public String getReconocimiento(){
+        return reconocimiento;
+    }
+    
+    public void setOrigen(String reconocimiento){
+        this.reconocimiento = reconocimiento;
+    }
+    public String getCategoria(){
+        return categoria;
+    }
+    
+    public void setCategoria(String categoria){
+        this.categoria = categoria;
+    }
+    
+    public String getProduccion(){
+        return producccion;
+    }
+    
+    public void setSigno(String producccion){
+        this.producccion = producccion;
+    }
+
 }
