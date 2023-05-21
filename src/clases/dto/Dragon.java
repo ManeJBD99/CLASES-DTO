84306b3eb200a1f277102742e9785764a9e73fce
ManeJBD99 <132291296+ManeJBD99@users.noreply.github.com>
2023-05-21 12:11:49 -0500

    Update Dragon.java

--- src/clases/dto/Dragon.java
+++ src/clases/dto/Dragon.java
@@ -9,5 +9,59 @@ package clases.dto;
  * @author manuc
  */
 public class Dragon {
+        private String tamaño;
+    private String color;
+    private String cultura;
+    private Integer edad;
+    private String alimento;
+    private String genero;
     
+    public String getTmaño(){
+        return tamaño;
+    }
+    
+    public void setTamaño (String tamaño){
+        this.tamaño = tamaño;
+    }
+    
+    public String getColor(){
+        return color;
+    }
+    
+    public void setColor(String color){
+        this.color = color;
+    }
+    
+    public String getCultura(){
+        return cultura;
+    }
+    
+    public void setCultura(String cultura){
+        this.cultura = cultura;
+    }
+    
+    public Integer getEdad(){
+        return edad;
+    }
+    
+    public void setEdad(Integer edad){
+        this.edad = edad;
+    }
+    
+    public String getAlimento(){
+        return alimento;
+    }
+    
+    public void setAlimento(String alimento){
+        this.alimento = alimento;
+    }
+    
+    public String getGenero(){
+        return genero;
+    }
+    
+    public void setGenero(String genero){
+        this.genero = genero;
+    }
+
 }
