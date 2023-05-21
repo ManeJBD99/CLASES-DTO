23c6e462d982f0ba79919f76631f170ddd98e30b
ManeJBD99 <132291296+ManeJBD99@users.noreply.github.com>
2023-05-21 11:49:53 -0500

    Update Vestimenta.java

--- src/clases/dto/Vestimenta.java
+++ src/clases/dto/Vestimenta.java
@@ -9,5 +9,59 @@ package clases.dto;
  * @author manuc
  */
 public class Vestimenta {
+        private String tela;
+    private String diseñador;
+    private String marca;
+    private String modelo;
+    private String talla;
+    private Integer costo;
     
+    public String getTela(){
+        return tela;
+    }
+    
+    public void setTela(String tela){
+        this.tela = tela;
+    }
+    
+    public String getDiseñador(){
+        return diseñador;
+    }
+    
+    public void setDiseñador(String diseñador){
+        this.diseñador = diseñador;
+    }
+    
+    public String getMarca(){
+        return marca;
+    }
+    
+    public void setMarca(String marca){
+        this.marca = marca;
+    }
+    
+    public String getModelo(){
+        return modelo;    
+    }
+    
+    public void setModelo(String modelo){
+        this.modelo = modelo;
+    }
+    
+    public String getTalla(){
+        return talla;
+    }
+    
+    public void setTalla(String talla){
+        this.talla = talla;
+    }
+    
+    public Integer getCosto(){
+        return costo;
+    }
+    
+    public void setCosrto(Integer costo){
+        this.costo = costo;
+    }
+
 }
