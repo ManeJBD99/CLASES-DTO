ab9096d9619c70bff65354f2da21d2a78d30c7b7
ManeJBD99 <132291296+ManeJBD99@users.noreply.github.com>
2023-05-21 11:59:20 -0500

    Update Cantante.java

--- src/clases/dto/Cantante.java
+++ src/clases/dto/Cantante.java
@@ -9,5 +9,57 @@ package clases.dto;
  * @author manuc
  */
 public class Cantante {
+        private String nombre;
+    private String apellido;
+    private Integer edad;
+    private String origen;
+    private String categoria;
+    private String signo;
     
+    public String getNombre(){
+        return nombre;
+    }
+    
+    public void setNombre(String nombre){
+        this.nombre = nombre;
+    }
+    
+    public String getApellido(){
+        return apellido;
+    }
+    public void setApellido(String apellido){
+        this.apellido = apellido;
+    }
+    
+    public Integer getEdad(){
+        return edad;
+    }
+    
+    public void setEdad(Integer edad){
+        this.edad = edad;
+    }
+    
+    public String getOrigen(){
+        return origen;
+    }
+    
+    public void setOrigen(String origen){
+        this.origen = origen;
+    }
+    public String getCategoria(){
+        return categoria;
+    }
+    
+    public void setCategoria(String categoria){
+        this.categoria = categoria;
+    }
+    
+    public String getSigno(){
+        return signo;
+    }
+    
+    public void setSigno(String signo){
+        this.signo = signo;
+    }
+ 
 }
